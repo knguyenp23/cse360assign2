3a6459fba37015c14ccb695c94c0892e67e131e7
kim <kim@192.168.0.76>
2019-10-08 22:36:04 -0700

    Initial version.

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,40 @@
+/*
+ * NAME: Kimberly Nguyen-Pham
+ * STUDENT ID: 1212667948
+ * COURSE: CSE360
+ * ASSIGNMENT: #2
+ * DESCRIPTION: AddingMachine allows the user to add to and subtract from a total.
+ * 				The history of operations performed can be printed to the user
+ * 				using the toString() method.
+ */
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
